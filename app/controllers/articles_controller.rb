fdeba5ae76fb5197a54c3fa70fe644b04bf8bdfb
RoosterRoo <desayan9@gmail.com>
2017-08-11 11:59:24 +0530

    Committed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
     def new
         @article = Article.new
     end
@@ -12,9 +15,31 @@ class ArticlesController < ApplicationController
         end
     end
 
-    def show
+    def edit
         @article = Article.find(params[:id])
     end
+
+    def show
+        @article = Article.find(params[:id]) 
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted!"
+        redirect_to articles_path
+    end
+
     private 
     def article_params
         params.require(:article).permit(:title,:description)
